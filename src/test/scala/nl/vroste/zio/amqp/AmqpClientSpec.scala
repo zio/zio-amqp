bd5b39cb982e8e31865e37ab24659a1a1edc61f8
Steven Vroonland <svroonland@gmail.com>
2020-07-26 12:14:19 +0200

    Formatting

--- src/test/scala/nl/vroste/zio/amqp/AmqpClientSpec.scala
+++ src/test/scala/nl/vroste/zio/amqp/AmqpClientSpec.scala
@@ -8,34 +8,34 @@ import zio.test._
 
 object AmqpClientSpec extends DefaultRunnableSpec {
 
-  override def spec = suite("AmqpClientSpec")(
-    testM("Amqp.consume delivers messages") {
-      val factory = new ConnectionFactory()
-      factory.setUri(URI.create(System.getenv("AMQP_SERVER_URI")))
+  override def spec =
+    suite("AmqpClientSpec")(
+      testM("Amqp.consume delivers messages") {
+        val factory = new ConnectionFactory()
+        factory.setUri(URI.create(System.getenv("AMQP_SERVER_URI")))
 
-      (Amqp
-        .connect(factory)
-        .tapM(_ => ZIO(println("Connected!"))) >>= Amqp.createChannel)
-        .tapM(_ => ZIO(println("Created channel!")))
-        .use {
-          channel =>
+        (Amqp
+          .connect(factory)
+          .tapM(_ => ZIO(println("Connected!"))) >>= Amqp.createChannel)
+          .tapM(_ => ZIO(println("Created channel!")))
+          .use { channel =>
             for {
               _ <- channel
-                    .consume(queue = System.getenv("AMQP_QUEUE"), consumerTag = "test")
-                    .mapM { record =>
-                      println(s"${record.getEnvelope.getDeliveryTag}: ${new String(record.getBody)}")
-                      ZIO.succeed(record.getEnvelope.getDeliveryTag)
-                    }
-                    .take(200)
-                    .aggregate(ZSink.collectAllN[Long](100))
-                    .map(_.last)
-                    .mapM { tag =>
-                      println(s"At tag: ${tag}")
-                      channel.ack(tag)
-                    }
-                    .runDrain
+                     .consume(queue = System.getenv("AMQP_QUEUE"), consumerTag = "test")
+                     .mapM { record =>
+                       println(s"${record.getEnvelope.getDeliveryTag}: ${new String(record.getBody)}")
+                       ZIO.succeed(record.getEnvelope.getDeliveryTag)
+                     }
+                     .take(200)
+                     .aggregate(ZSink.collectAllN[Long](100))
+                     .map(_.last)
+                     .mapM { tag =>
+                       println(s"At tag: ${tag}")
+                       channel.ack(tag)
+                     }
+                     .runDrain
             } yield assertCompletes
-        }
-    }
-  )
+          }
+      }
+    )
 }
