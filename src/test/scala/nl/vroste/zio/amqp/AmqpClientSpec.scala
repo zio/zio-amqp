ec27eddfc3c889e7a68373f6a2a361367930a80c
Erik Innocent <erik.innocent@rallyhealth.com>
2021-08-18 20:10:42 -0700

    Improving unit tests (issue 146)

--- src/test/scala/nl/vroste/zio/amqp/AmqpClientSpec.scala
+++ src/test/scala/nl/vroste/zio/amqp/AmqpClientSpec.scala
@@ -1,18 +1,28 @@
 package nl.vroste.zio.amqp
-import java.net.URI
-
 import com.rabbitmq.client.ConnectionFactory
 import zio.ZIO
+import zio.duration.Duration
+import zio.test.Assertion.equalTo
+import zio.test.TestAspect._
 import zio.test._
-import zio.stream.ZTransducer
+
+import java.net.URI
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 object AmqpClientSpec extends DefaultRunnableSpec {
 
   override def spec =
     suite("AmqpClientSpec")(
       testM("Amqp.consume delivers messages") {
-        val factory = new ConnectionFactory()
-        val uri     = URI.create(Option(System.getenv("AMQP_SERVER_URI")).getOrElse("amqp://guest:guest@localhost:5672"))
+        val testAmqpSuffix = s"AmqpClientSpec-${UUID.randomUUID().toString}"
+        val exchangeName   = s"exchange-$testAmqpSuffix"
+        val queueName      = s"queue-$testAmqpSuffix"
+        val message1       = UUID.randomUUID().toString
+        val message2       = UUID.randomUUID().toString
+        val messages       = Set(message1, message2)
+        val factory        = new ConnectionFactory()
+        val uri            = URI.create(Option(System.getenv("AMQP_SERVER_URI")).getOrElse("amqp://guest:guest@localhost:5672"))
         println(uri)
         factory.setUri(uri)
 
@@ -22,27 +32,29 @@ object AmqpClientSpec extends DefaultRunnableSpec {
           .tapM(_ => ZIO(println("Created channel!")))
           .use { channel =>
             for {
-              _ <- channel.queueDeclare("queue1")
-              _ <- channel.exchangeDeclare("exchange1", ExchangeType.Fanout)
-              _ <- channel.queueBind("queue1", "exchange1", "myroutingkey")
-              _ <- channel.publish("exchange1", "message1".getBytes)
-              _ <- channel.publish("exchange1", "message2".getBytes)
-              _ <- channel
-                     .consume(queue = "queue1", consumerTag = "test")
-                     .mapM { record =>
-                       println(s"${record.getEnvelope.getDeliveryTag}: ${new String(record.getBody)}")
-                       ZIO.succeed(record.getEnvelope.getDeliveryTag)
-                     }
-                     .take(2)
-                     .aggregate(ZTransducer.collectAllN[Long](100))
-                     .map(_.last)
-                     .mapM { tag =>
-                       println(s"At tag: ${tag}")
-                       channel.ack(tag)
-                     }
-                     .runDrain
-            } yield assertCompletes
+              _ <- channel.queueDeclare(queueName)
+              _ <- channel.exchangeDeclare(exchangeName, ExchangeType.Fanout)
+              _ <- channel.queueBind(queueName, exchangeName, "myroutingkey")
+              _ <- channel.publish(exchangeName, message1.getBytes)
+              _ <- channel.publish(exchangeName, message2.getBytes)
+              bodies <- channel
+                .consume(queue = queueName, consumerTag = "test")
+                .mapM { record =>
+                  println(s"${record.getEnvelope.getDeliveryTag}: ${new String(record.getBody)}")
+                  ZIO.succeed(record)
+                }
+                .take(2)
+                .runCollect
+                .tap { records =>
+                  val tag = records.last.getEnvelope.getDeliveryTag
+                  println(s"At tag: $tag")
+                  channel.ack(tag)
+
+                }
+                .map(_.map(r => new String(r.getBody)))
+            } yield
+              assert(messages)(equalTo(bodies.toSet))
           }
-      }
+      } @@ timeout(Duration(10, TimeUnit.SECONDS))
     )
 }
