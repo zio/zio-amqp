49ce33ea361b18ad422497584c1d7826189019b9
Scala Steward <me@scala-steward.org>
2021-09-10 03:29:18 +0200

    Update logback-classic to 1.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
     "dev.zio"                %% "zio-interop-reactivestreams" % "1.3.5",
     "com.rabbitmq"            % "amqp-client"                 % "5.13.0",
-    "ch.qos.logback"          % "logback-classic"             % "1.2.5"    % Test,
+    "ch.qos.logback"          % "logback-classic"             % "1.2.6"    % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.5.0",
     "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC6"
   )
