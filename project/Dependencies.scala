f6f2e2fca4a11458505a3c93263529b97f864505
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-15 15:29:30 +0200

    Update zio-interop-reactivestreams to 1.3.8 (#170)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "dev.zio"                %% "zio-streams"                 % zioVersion,
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
-    "dev.zio"                %% "zio-interop-reactivestreams" % "1.3.7",
+    "dev.zio"                %% "zio-interop-reactivestreams" % "1.3.8",
     "com.rabbitmq"            % "amqp-client"                 % "5.13.0",
     "ch.qos.logback"          % "logback-classic"             % "1.2.6"    % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.5.0",
