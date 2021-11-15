2ced5b55ddc82e22381cd1ec2866c6b73b7090d4
Scala Steward <me@scala-steward.org>
2021-11-15 13:06:14 +0100

    Update amqp-client to 5.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
     "dev.zio"                %% "zio-interop-reactivestreams" % "1.3.8",
-    "com.rabbitmq"            % "amqp-client"                 % "5.13.1",
+    "com.rabbitmq"            % "amqp-client"                 % "5.14.0",
     "ch.qos.logback"          % "logback-classic"             % "1.2.6"    % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.5.0",
     "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC6"
