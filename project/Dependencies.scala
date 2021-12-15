ba67f5f76a27e4455d74a9ee66be8a640f6b778b
Scala Steward <me@scala-steward.org>
2021-12-15 05:39:38 +0100

    Update zio-streams, zio-test, zio-test-sbt to 2.0.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val zioVersion = "2.0.0-M4"
+  val zioVersion = "2.0.0-RC1"
   lazy val deps  = Seq(
     "dev.zio"                %% "zio-streams"                 % zioVersion,
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
