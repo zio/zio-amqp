dd47e2cd1c85fb3fb4c0ea12147e29cbf2cdb48e
Devan Carlson <devansc@gmail.com>
2022-01-12 09:03:37 -0800

    Exchange declare with custom type (#211)
    * Add exchangeDeclare0 with String
    
    * svroonland's suggestion
    
    * format

--- src/main/scala/nl/vroste/zio/amqp/model/ExchangeType.scala
+++ src/main/scala/nl/vroste/zio/amqp/model/ExchangeType.scala
@@ -5,17 +5,20 @@ sealed trait ExchangeType extends Product with Serializable {
 }
 
 object ExchangeType {
-  case object Direct  extends ExchangeType {
+  case object Direct                extends ExchangeType {
     override val name: String = "direct"
   }
-  case object Fanout  extends ExchangeType {
+  case object Fanout                extends ExchangeType {
     override val name: String = "fanout"
   }
-  case object Topic   extends ExchangeType {
+  case object Topic                 extends ExchangeType {
     override val name: String = "topic"
   }
-  case object Headers extends ExchangeType {
+  case object Headers               extends ExchangeType {
     override val name: String = "headers"
+  }
+  case class Custom(`type`: String) extends ExchangeType {
+    override val name: String = `type`
   }
 
   implicit def represent(`type`: ExchangeType): String = `type`.name
