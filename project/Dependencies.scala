51b607017ec76d64ca93c183c948c4f06caab8b0
Steven Vroonland <steven@dataworkz.nl>
2022-07-21 20:40:52 +0200

    Bump zio-interactive-streams and zio-prelude to latest version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,10 +7,10 @@ object Dependencies {
     "dev.zio"                %% "zio-streams"                 % zioVersion,
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
-    "dev.zio"                %% "zio-interop-reactivestreams" % "1.3.10",
+    "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.0",
     "com.rabbitmq"            % "amqp-client"                 % "5.15.0",
     "ch.qos.logback"          % "logback-classic"             % "1.2.11"   % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.8.0",
-    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC13"
+    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC15"
   )
 }
