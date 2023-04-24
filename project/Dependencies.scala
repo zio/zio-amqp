43009fd1ea024dcc12e8ffbc174afb938badf72f
Dan Aharon <dan@aharon.dev>
2023-04-24 16:01:12 -0700

    Updated dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,16 @@ import sbt._
 
 object Dependencies {
 
-  val zioVersion = "2.0.2"
+  val zioVersion = "2.0.13"
   lazy val deps  = Seq(
+    "dev.zio"                %% "zio"                         % zioVersion,
     "dev.zio"                %% "zio-streams"                 % zioVersion,
+    "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.1",
+    "com.rabbitmq"            % "amqp-client"                 % "5.16.0",
+    "org.scala-lang.modules" %% "scala-collection-compat"     % "2.8.1",
+    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC18",
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
-    "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.0",
-    "com.rabbitmq"            % "amqp-client"                 % "5.16.0",
-    "ch.qos.logback"          % "logback-classic"             % "1.2.11"   % Test,
-    "org.scala-lang.modules" %% "scala-collection-compat"     % "2.8.1",
-    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC15"
+    "ch.qos.logback"          % "logback-classic"             % "1.4.6"    % Test
   )
 }
