3fd414ffa58ae515df5ac5acef047aa8fd200343
svroonland <svroonland@users.noreply.github.com>
2023-06-25 11:28:44 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,10 +7,10 @@ object Dependencies {
     "dev.zio"                %% "zio-streams"                 % zioVersion,
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
-    "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.0",
+    "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.2",
     "com.rabbitmq"            % "amqp-client"                 % "5.16.0",
     "ch.qos.logback"          % "logback-classic"             % "1.2.11"   % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.8.1",
-    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC15"
+    "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC19"
   )
 }
