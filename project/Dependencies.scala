23b211030725abd22247912fd68fce32ee9262b7
Scala Steward <scala_steward@virtuslab.com>
2023-08-12 00:36:16 +0000

    Update logback-classic to 1.4.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
     "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.2",
     "com.rabbitmq"            % "amqp-client"                 % "5.18.0",
-    "ch.qos.logback"          % "logback-classic"             % "1.4.8"    % Test,
+    "ch.qos.logback"          % "logback-classic"             % "1.4.11"   % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.11.0",
     "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC19"
   )
