5437a38370d349f1c8a442f6d978c425e6079376
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 03:39:53 +0000

    Update amqp-client to 5.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "dev.zio"                %% "zio-test"                    % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                % zioVersion % Test,
     "dev.zio"                %% "zio-interop-reactivestreams" % "2.0.2",
-    "com.rabbitmq"            % "amqp-client"                 % "5.18.0",
+    "com.rabbitmq"            % "amqp-client"                 % "5.19.0",
     "ch.qos.logback"          % "logback-classic"             % "1.4.8"    % Test,
     "org.scala-lang.modules" %% "scala-collection-compat"     % "2.11.0",
     "dev.zio"                %% "zio-prelude"                 % "1.0.0-RC19"
