6b892791e5875e976a09fa488f7e34ad5446f063
Milad Khajavi <khajavi@gmail.com>
2023-11-17 19:16:11 +0330

    add sidebar.

---
+++ docs/sidebars.js
@@ -0,0 +1,14 @@
+const sidebars = {
+  sidebar: [
+    {
+      type: "category",
+      label: "ZIO AMQP",
+      collapsed: false,
+      link: { type: "doc", id: "index" },
+      items: [ 
+      ]
+    }
+  ]
+};
+
+module.exports = sidebars;
