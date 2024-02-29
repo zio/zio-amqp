a0fcf02ef7a86da85f20a0acb1a115e3ef132f14
Scala Steward <scala_steward@virtuslab.com>
2024-02-29 23:50:04 +0000

    Update logback-classic to 1.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     "com.dimafeng"           %% "testcontainers-scala-rabbitmq" % "0.41.0"   % Test,
     "dev.zio"                %% "zio-test"                      % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                  % zioVersion % Test,
-    "ch.qos.logback"          % "logback-classic"               % "1.4.14"   % Test,
+    "ch.qos.logback"          % "logback-classic"               % "1.5.1"    % Test,
     "org.scalameta"           % "semanticdb-scalac_2.13.12"     % "4.8.14"
   )
 }
