f0e69d37c89f35d421e51d31d6d56b6ff80575e4
Scala Steward <scala_steward@virtuslab.com>
2024-03-30 23:37:09 +0000

    Update semanticdb-scalac_2.13.12 to 4.8.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,6 +13,6 @@ object Dependencies {
     "dev.zio"                %% "zio-test"                      % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                  % zioVersion % Test,
     "ch.qos.logback"          % "logback-classic"               % "1.5.3"    % Test,
-    "org.scalameta"           % "semanticdb-scalac_2.13.12"     % "4.8.14"
+    "org.scalameta"           % "semanticdb-scalac_2.13.12"     % "4.8.15"
   )
 }
