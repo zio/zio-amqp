4db5e1f6ae70570787ae00dea3be3d47c3899a55
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-01 09:56:57 +0200

    Update amqp-client to 5.21.0 (#440)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val deps  = Seq(
     "dev.zio"                %% "zio-streams"                   % zioVersion,
     "dev.zio"                %% "zio-interop-reactivestreams"   % "2.0.2",
-    "com.rabbitmq"            % "amqp-client"                   % "5.20.0",
+    "com.rabbitmq"            % "amqp-client"                   % "5.21.0",
     "org.scala-lang.modules" %% "scala-collection-compat"       % "2.11.0",
     "dev.zio"                %% "zio-prelude"                   % "1.0.0-RC23",
     "com.dimafeng"           %% "testcontainers-scala-rabbitmq" % "0.41.4"   % Test,
