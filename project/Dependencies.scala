7a12bdae698c505eea6973e5cd572d6d4943c38c
Scala Steward <scala_steward@virtuslab.com>
2024-12-19 22:24:46 +0000

    Update testcontainers-scala-rabbitmq to 0.41.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "com.rabbitmq"            % "amqp-client"                   % "5.21.0",
     "org.scala-lang.modules" %% "scala-collection-compat"       % "2.11.0",
     "dev.zio"                %% "zio-prelude"                   % "1.0.0-RC23",
-    "com.dimafeng"           %% "testcontainers-scala-rabbitmq" % "0.41.4"   % Test,
+    "com.dimafeng"           %% "testcontainers-scala-rabbitmq" % "0.41.5"   % Test,
     "dev.zio"                %% "zio-test"                      % zioVersion % Test,
     "dev.zio"                %% "zio-test-sbt"                  % zioVersion % Test,
     "ch.qos.logback"          % "logback-classic"               % "1.5.6"    % Test,
